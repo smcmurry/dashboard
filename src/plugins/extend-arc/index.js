1a660c9cbd58951e0768a7b8652a5c139c400d3b
Jarod McMurry <smcmurry@mymail.mines.edu>
2022-06-07 15:01:09 -0600

    print error

--- src/plugins/extend-arc/index.js
+++ src/plugins/extend-arc/index.js
@@ -3,7 +3,7 @@ module.exports = {
     deploy: {
         start: async function ({ arc, cloudformation, dryRun, inventory, stage }) {
             //cloudformation.Resources.csfsAnyCatchallHTTPLambda = cloudformation.Resources.AnyCatchallHTTPLambda
-            print(cloudformation.Resources)
+            console.log(cloudformation.Resources)
             return cloudformation
         }
     }
